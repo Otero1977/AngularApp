b2dcdbcce9438be0e23142e671c58d6897226f41
Otero1977 <108960858+Otero1977@users.noreply.github.com>
2022-12-05 01:16:07 -0300

    Segundo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { AcercaComponent } from './componentes/acerca/acerca.component';
 import { BarnavegacionComponent } from './componentes/barnavegacion/barnavegacion.component';
 import { BienvenidaComponent } from './componentes/bienvenida/bienvenida.component';
 import { ExplaboralComponent } from './componentes/explaboral/explaboral.component';
-import { FormAcademicaComponent } from './componentes/form-academica/form-academica.component';
 import { SkillsComponent } from './componentes/skills/skills.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { FormacademicaComponent } from './componentes/formacademica/formacademica.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { PortfolioService } from './servicios/portfolio.service';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
@@ -18,14 +23,25 @@ import { FormacademicaComponent } from './componentes/formacademica/formacademic
     BarnavegacionComponent,
     BienvenidaComponent,
     ExplaboralComponent,
-    FormAcademicaComponent,
+    FormacademicaComponent,
     SkillsComponent,
     ProyectosComponent,
     FormacademicaComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#297dd7",
+      innerStrokeColor: "#103704",
+      animationDuration: 300,
+    })
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
